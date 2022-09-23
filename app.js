49c68d521faafbdc471db3973ae7d3c7ec28c37c
Jesus Salomon <salomonj1121@gmail.com>
2022-09-23 02:18:06 -0500

    added joi for data validation

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const morgan = require('morgan');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
 
 mongoose.connect('mongodb://localhost:27017/rate-my-campsite', {
   useNewUrlParser: true,
@@ -48,8 +49,26 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground)
-      throw new ExpressError('Invalid Campground Data', 400);
+    // if (!req.body.campground)
+    //   throw new ExpressError('Invalid Campground Data', 400);
+
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required,
+        price: Joi.number().required.min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+      }).required(),
+    });
+
+    const { error } = campgroundSchema.validate(req.body);
+
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(',');
+      throw new ExpressError(msg, 400);
+    }
+
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
