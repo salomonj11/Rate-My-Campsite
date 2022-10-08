eeb5501df27d0c63a74236914aab583275079c1e
Jesus Salomon <salomonj1121@gmail.com>
2022-10-08 14:07:30 -0500

    fixed some security issues

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const ExpressError = require('./utils/ExpressError');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
@@ -40,6 +41,7 @@ app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
   secret: 'thisisasecret',
